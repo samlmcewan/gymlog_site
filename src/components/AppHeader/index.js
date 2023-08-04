638a15e1ec3679dcc8ef14c996c4b2ce85fb88d7
samlmcewan <samlmcewan@gmail.com>
2023-08-04 22:17:24 +0100

    Update AppHeader

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -12,7 +12,7 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
             <div className='app-title-text'>
-              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <h1 className='app-title'>gym.log()</h1>
               <p className='app-intro'>
                 Using FaunaDB & Netlify functions
               </p>
@@ -20,19 +20,13 @@ const AppHeader = (props) => {
           </div>
         </div>
         <div className='deploy-button-wrapper'>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>
-            <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
-          </a>
           <div className='view-src'>
             <a
               target='_blank'
               rel='noopener noreferrer'
               href='https://github.com/netlify/netlify-faunadb-example'>
               <img className='github-icon' src={github} alt='view repo on github' />
-              View the source Luke
+              View the source samlmcewan
             </a>
           </div>
         </div>
